5e48fe2664b377fe58ff3d630cc6d893c94555aa
gebhart25 <77856708+gebhart25@users.noreply.github.com>
2021-02-05 12:37:53 -0600

    Add files via upload

--- regfile_tb.sv
+++ regfile_tb.sv
@@ -1,10 +1,10 @@
 module stimulus();
 	logic clk;
 	logic we3;
-	logic ra1, ra2, wa3;
-	logic wd3;
-	logic rd1, rd2;
-
+	logic [4:0] ra1, ra2, wa3;
+	logic [31:0] rd1, rd2, wd3;
+	logic Out;
+	
 	integer handle3;
 	integer desc3;
 
@@ -34,13 +34,39 @@ module stimulus();
 	always
 		begin
 			desc3 = handle3;
-			#5 $fdisplay(desc3, "%b %b || %b", we3, ra1, ra2, rd1, wa3, wd3, rd2);
+			#5 $fdisplay(desc3, "%b %b", we3,clk);
 		end
 	initial
 		begin
-			/*
-				VALUES
-				*/
+			//beginning with all values set to 0
+			#0 ra1 = 5'h0;
+			#0 ra2 = 5'h0;
+			#0 we3 = 1'b0;
+			#0 wa3 = 5'h0;
+			#0 wd3 = 32'h0;
+			
+			//on first positive edge
+			#9 wd3 = 32'hFFFF_FFFF;
+			wa3 = 5'h1;
+			ra1 = 5'h1;
+			we3 = 1'b1;
+			
+			//test if wd write when bit is 0
+			#10 we3 = 1'b0;
+			wd3 = 32'h2525_2525;
+			
+			//writing the array to a different register
+			#10 wa3 = 5'h2;
+			ra2 = 5'h2;
+			we3 = 1'b1;
+			
+			//test writing to register 0 (should fail)
+			#10 wa3 = 5'h0;
+			ra1 = 5'h0;
+			
+			//read updates on positive clk edge
+			#10 wa3 = 5'h2;
+			wd3 = 32'h1234_5678;
 		end
 endmodule //regfile_tb
 
