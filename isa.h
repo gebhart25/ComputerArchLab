b074acb704cdb6095401e1fbb9cd47842dbbdfbb
gebhart25 <77856708+gebhart25@users.noreply.github.com>
2021-02-11 21:45:12 -0600

    Add files via upload

--- isa.h
+++ isa.h
@@ -86,15 +86,186 @@ int ADD (int Rd, int Rn, int Operand2, int I, int S, int CC) {
 
 }
 
+int AND (int Rd, int Rn, int Operand2, int I, int S, int CC) {
+
+  int cur = 0;	//cur: output
+  if(I == 0) {
+    int sh = (Operand2 & 0x00000060) >> 5;
+    int shamt5 = (Operand2 & 0x00000F80) >> 7;
+    int bit4 = (Operand2 & 0x00000010) >> 4;
+    int Rm = Operand2 & 0x0000000F;
+    int Rs = (Operand2 & 0x00000F00) >> 8;
+    if (bit4 == 0) 
+      switch (sh) {
+      case 0: cur = CURRENT_STATE.REGS[Rn] & 
+	  (CURRENT_STATE.REGS[Rm] << shamt5);
+	  break;
+      case 1: cur = CURRENT_STATE.REGS[Rn] & 
+	  (CURRENT_STATE.REGS[Rm] >> shamt5);
+	  break;
+      case 2: cur = CURRENT_STATE.REGS[Rn] & 
+	  (CURRENT_STATE.REGS[Rm] >> shamt5);
+    	  break;
+      case 3: cur = CURRENT_STATE.REGS[Rn] & 
+	      ((CURRENT_STATE.REGS[Rm] >> shamt5) |
+               (CURRENT_STATE.REGS[Rm] << (32 - shamt5)));
+	  break;
+      }     
+    else
+      switch (sh) {
+      case 0: cur = CURRENT_STATE.REGS[Rn] & 
+	  (CURRENT_STATE.REGS[Rm] << CURRENT_STATE.REGS[Rs]);
+	  break;
+      case 1: cur = CURRENT_STATE.REGS[Rn] & 
+	  (CURRENT_STATE.REGS[Rm] >> CURRENT_STATE.REGS[Rs]);
+	  break;
+      case 2: cur = CURRENT_STATE.REGS[Rn] & 
+	  (CURRENT_STATE.REGS[Rm] >> CURRENT_STATE.REGS[Rs]);
+	  break;
+      case 3: cur = CURRENT_STATE.REGS[Rn] & 
+	      ((CURRENT_STATE.REGS[Rm] >> CURRENT_STATE.REGS[Rs]) |
+               (CURRENT_STATE.REGS[Rm] << (32 - CURRENT_STATE.REGS[Rs])));
+	  break;
+      }      
+  }
+  if (I == 1) {
+    int rotate = Operand2 >> 8;
+    int Imm = Operand2 & 0x000000FF;
+    cur = CURRENT_STATE.REGS[Rn] & (Imm>>2*rotate|(Imm<<(32-2*rotate)));
+  }
+  NEXT_STATE.REGS[Rd] = cur;
+  if (S == 1) {
+    if (cur < 0)
+      NEXT_STATE.CPSR |= N_N;
+    if (cur == 0)
+      NEXT_STATE.CPSR |= Z_N;
+  }	
+  return 0;
+
+}
+
+int EOR (int Rd, int Rn, int Operand2, int I, int S, int CC) {
+
+  int cur = 0;	//cur: output
+  if(I == 0) {
+    int sh = (Operand2 & 0x00000060) >> 5;
+    int shamt5 = (Operand2 & 0x00000F80) >> 7;
+    int bit4 = (Operand2 & 0x00000010) >> 4;
+    int Rm = Operand2 & 0x0000000F;
+    int Rs = (Operand2 & 0x00000F00) >> 8;
+    if (bit4 == 0) 
+      switch (sh) {
+      case 0: cur = CURRENT_STATE.REGS[Rn] ^ 
+	  (CURRENT_STATE.REGS[Rm] << shamt5);
+	  break;
+      case 1: cur = CURRENT_STATE.REGS[Rn] ^ 
+	  (CURRENT_STATE.REGS[Rm] >> shamt5);
+	  break;
+      case 2: cur = CURRENT_STATE.REGS[Rn] ^ 
+	  (CURRENT_STATE.REGS[Rm] >> shamt5);
+    	  break;
+      case 3: cur = CURRENT_STATE.REGS[Rn] ^ 
+	      ((CURRENT_STATE.REGS[Rm] >> shamt5) |
+               (CURRENT_STATE.REGS[Rm] << (32 - shamt5)));
+	  break;
+      }     
+    else
+      switch (sh) {
+      case 0: cur = CURRENT_STATE.REGS[Rn] ^ 
+	  (CURRENT_STATE.REGS[Rm] << CURRENT_STATE.REGS[Rs]);
+	  break;
+      case 1: cur = CURRENT_STATE.REGS[Rn] ^ 
+	  (CURRENT_STATE.REGS[Rm] >> CURRENT_STATE.REGS[Rs]);
+	  break;
+      case 2: cur = CURRENT_STATE.REGS[Rn] ^ 
+	  (CURRENT_STATE.REGS[Rm] >> CURRENT_STATE.REGS[Rs]);
+	  break;
+      case 3: cur = CURRENT_STATE.REGS[Rn] ^ 
+	      ((CURRENT_STATE.REGS[Rm] >> CURRENT_STATE.REGS[Rs]) |
+               (CURRENT_STATE.REGS[Rm] << (32 - CURRENT_STATE.REGS[Rs])));
+	  break;
+      }      
+  }
+  if (I == 1) {
+    int rotate = Operand2 >> 8;
+    int Imm = Operand2 & 0x000000FF;
+    cur = CURRENT_STATE.REGS[Rn] ^ (Imm>>2*rotate|(Imm<<(32-2*rotate)));
+  }
+  NEXT_STATE.REGS[Rd] = cur;
+  if (S == 1) {
+    if (cur < 0)
+      NEXT_STATE.CPSR |= N_N;
+    if (cur == 0)
+      NEXT_STATE.CPSR |= Z_N;
+  }	
+  return 0;
+
+}
+
+int ORR (int Rd, int Rn, int Operand2, int I, int S, int CC) {
+
+  int cur = 0;	//cur: output
+  if(I == 0) {
+    int sh = (Operand2 & 0x00000060) >> 5;
+    int shamt5 = (Operand2 & 0x00000F80) >> 7;
+    int bit4 = (Operand2 & 0x00000010) >> 4;
+    int Rm = Operand2 & 0x0000000F;
+    int Rs = (Operand2 & 0x00000F00) >> 8;
+    if (bit4 == 0) 
+      switch (sh) {
+      case 0: cur = CURRENT_STATE.REGS[Rn] | 
+	  (CURRENT_STATE.REGS[Rm] << shamt5);
+	  break;
+      case 1: cur = CURRENT_STATE.REGS[Rn] | 
+	  (CURRENT_STATE.REGS[Rm] >> shamt5);
+	  break;
+      case 2: cur = CURRENT_STATE.REGS[Rn] | 
+	  (CURRENT_STATE.REGS[Rm] >> shamt5);
+    	  break;
+      case 3: cur = CURRENT_STATE.REGS[Rn] | 
+	      ((CURRENT_STATE.REGS[Rm] >> shamt5) |
+               (CURRENT_STATE.REGS[Rm] << (32 - shamt5)));
+	  break;
+      }     
+    else
+      switch (sh) {
+      case 0: cur = CURRENT_STATE.REGS[Rn] | 
+	  (CURRENT_STATE.REGS[Rm] << CURRENT_STATE.REGS[Rs]);
+	  break;
+      case 1: cur = CURRENT_STATE.REGS[Rn] | 
+	  (CURRENT_STATE.REGS[Rm] >> CURRENT_STATE.REGS[Rs]);
+	  break;
+      case 2: cur = CURRENT_STATE.REGS[Rn] | 
+	  (CURRENT_STATE.REGS[Rm] >> CURRENT_STATE.REGS[Rs]);
+	  break;
+      case 3: cur = CURRENT_STATE.REGS[Rn] | 
+	      ((CURRENT_STATE.REGS[Rm] >> CURRENT_STATE.REGS[Rs]) |
+               (CURRENT_STATE.REGS[Rm] << (32 - CURRENT_STATE.REGS[Rs])));
+	  break;
+      }      
+  }
+  if (I == 1) {
+    int rotate = Operand2 >> 8;
+    int Imm = Operand2 & 0x000000FF;
+    cur = CURRENT_STATE.REGS[Rn] | (Imm>>2*rotate|(Imm<<(32-2*rotate)));
+  }
+  NEXT_STATE.REGS[Rd] = cur;
+  if (S == 1) {
+    if (cur < 0)
+      NEXT_STATE.CPSR |= N_N;
+    if (cur == 0)
+      NEXT_STATE.CPSR |= Z_N;
+  }	
+  return 0;
+
+}
 int ADC (char* i_);
-int AND (char* i_);
 int ASR (char* i_);
 int B (char* i_);
 int BIC (char* i_);
 int BL (char* i_);
 int CMN (char* i_);
 int CMP (char* i_);
-int EOR (char* i_);
 int LDR (char* i_);
 int LDRB (char* i_);
 int LSL (char* i_);
@@ -103,7 +274,6 @@ int MLA (char* i_);
 int MOV (char* i_);
 int MUL (char* i_);
 int MVN (char* i_);
-int ORR (char* i_);
 int ROR (char* i_);
 int SBC (char* i_);
 int STR (char* i_);
